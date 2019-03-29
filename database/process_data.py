a2bbef0f190a8a6cb24d9e4fa0fc9316b2fd532e
André F. Martins <martins.adw@gmail.com>
2019-03-29 15:13:15 -0300

    Utilizar o KNeighborsRegressor e o GradientBoostingRegressor

--- database/process_data.py
+++ database/process_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn import neighbors, ensemble
 
 
 def get_distances_matrix(data, distance_function, *args, discretization=None, **kwargs):
@@ -17,26 +18,57 @@ def get_distances_matrix(data, distance_function, *args, discretization=None, **
     return distance_matrix
 
 
-def get_grades_prediction(students_grades, suggestions_distances, prediction_type='weighted', quant_similar=3):
+def get_grades_prediction(students_grades, suggestions_distances, prediction_weight='distance', quant_similar=3):
     quant_students = len(students_grades)
     grade_prediction = np.empty((quant_students,))
 
-    for i in range(quant_students):
-        distances = np.concatenate((suggestions_distances[i, :i], suggestions_distances[i, i+1:]))
-        grades = np.concatenate((students_grades[:i], students_grades[i+1:]))
-        sorted_indices = np.argsort(distances)
+    # print(students_grades.tolist())
+    # print("------------------")
+    # print(suggestions_distances.tolist())
+    # print("==================")
 
-        similar_distances = distances[sorted_indices[:quant_similar]]
-        similar_grades = grades[sorted_indices[:quant_similar]]
+    if prediction_weight == 'kneighbors':
+        distances = suggestions_distances.copy()
+        np.fill_diagonal(distances, 1000)
 
-        if prediction_type == 'mean':
-            grade_prediction[i] = similar_grades.mean()
-        elif prediction_type == 'weighted':
-            grade_prediction[i] = np.average(similar_grades, weights=(1 / (similar_distances+0.01)))
+        knn = neighbors.KNeighborsRegressor(quant_similar, metric='precomputed')
+        grade_prediction[:] = knn.fit(suggestions_distances, students_grades).predict(distances)
+    else:
+        for i in range(quant_students):
+            distances = np.concatenate((suggestions_distances[i, :i], suggestions_distances[i, i+1:]))
+            grades = np.concatenate((students_grades[:i], students_grades[i+1:]))
 
+            # distances = suggestions_distances[i].copy()
+            # distances[i] = 1000
+            # grades = students_grades.copy()
+
+            sorted_indices = np.argsort(distances)
+
+            similar_distances = distances[sorted_indices[:quant_similar]]
+            similar_grades = grades[sorted_indices[:quant_similar]]
+
+            if prediction_weight == 'uniform':
+                grade_prediction[i] = similar_grades.mean()
+            elif prediction_weight == 'distance':
+                # grade_prediction[i] = np.average(similar_grades, weights=(1 / (similar_distances+0.01)))
+                grade_prediction[i] = np.average(similar_grades, weights=(1 - similar_distances + 0.01))
 
     return grade_prediction
 
+
+def get_grades_regression(students_suggestions, students_grades, prediction_weight='distance', quant_similar=3):
+    quant_students = len(students_grades)
+    grade_prediction = np.empty((quant_students,))
+
+    print(students_suggestions)
+    if prediction_weight == 'kneighbors':
+        knn = neighbors.KNeighborsRegressor(quant_similar, metric='matching')
+        grade_prediction[:] = knn.fit(students_suggestions, students_grades).predict(students_suggestions)
+    elif prediction_weight == 'gradient':
+        clf = ensemble.GradientBoostingRegressor()
+        grade_prediction[:] = clf.fit(students_suggestions, students_grades).predict(students_suggestions)
+
+    return grade_prediction
 
 def DEBUG_get_grades_prediction(students_grades, suggestions_distances, students_id, quant_similar=3):
     quant_students = len(students_grades)
--- tests/prediction.py
+++ tests/prediction.py
@@ -5,20 +5,23 @@ from database.read import open_students, open_suggestions, open_grades, open_abi
 from database.students import get_students_index, get_students_id
 from database.filters import filter_students_by_id, filter_students_by_style
 from database.get_data import get_students_suggestions, get_students_abilities, get_students_grades, get_students_styles
-from database.process_data import get_distances_matrix, get_grades_prediction, DEBUG_get_grades_prediction
+from database.process_data import get_distances_matrix, get_grades_prediction, get_grades_regression, DEBUG_get_grades_prediction
 
 from utils.misc import hamming_distance, cosine_distance, array_interleave
 from utils.output import save_csv
 
-def prediction(subject, distance_name, prediction_type, value_used, style_filter, quant_similar, out_info=None):
+
+base_path = 'results/2019-03-27 - Teste/'
+
+def prediction(subject, distance_name, prediction_weight, value_used, style_filter, quant_similar, out_info=None):
     ##PARAMS########################################################################
     graphic_type = 'scatter'  # ['scatter', 'difference', 'values', 'none']
     graphic_save = True
     graphic_show = False
-    graphics_folder_name = 'results/2019-03-25 - Média e desvio/graphics'
+    graphics_folder_name = base_path + 'graphics'
 
     csv_type = 'values'  # ['values', 'none']
-    csv_folder_name = 'results/2019-03-25 - Média e desvio/csv'
+    csv_folder_name = base_path + 'csv'
     ################################################################################
     if distance_name == 'hamming':
         distance_show_name = 'Hamming'
@@ -78,7 +81,13 @@ def prediction(subject, distance_name, prediction_type, value_used, style_filter
         students_values = filtered_grades - filtered_abilities
 
     # Calcula a previsão do valor utilizado
-    grades_prediction = get_grades_prediction(students_values, filtered_distances, prediction_type=prediction_type, quant_similar=quant_similar)
+    # if prediction_weight in ('kneighbors', 'gradient'):
+    #     filtered_suggestions = students_suggestions[~students_grades.mask]
+    #     grades_prediction = get_grades_regression(filtered_suggestions, students_values, prediction_weight=prediction_weight, quant_similar=quant_similar)
+    # else:
+    #     grades_prediction = get_grades_prediction(students_values, filtered_distances, prediction_weight=prediction_weight, quant_similar=quant_similar)
+    grades_prediction = get_grades_prediction(students_values, filtered_distances, prediction_weight=prediction_weight, quant_similar=quant_similar)
+
 
     grades_difference = np.abs(grades_prediction - students_values)
 
@@ -107,10 +116,12 @@ def prediction(subject, distance_name, prediction_type, value_used, style_filter
         out_info['correlation'] = values_correlation
         out_info['mean_difference'] = values_mean
         out_info['error'] = values_error
-        out_info['mean_grade'] = np.mean(grades_prediction)
-        out_info['deviation_grade'] = np.std(grades_prediction)
-        out_info['mean_prediction'] = np.mean(students_values)
-        out_info['deviation_prediction'] = np.std(students_values)
+        out_info['mean_ability'] = np.mean(filtered_abilities)
+        out_info['deviation_ability'] = np.std(filtered_abilities)
+        out_info['mean_grade'] = np.mean(filtered_grades)
+        out_info['deviation_grade'] = np.std(filtered_grades)
+        out_info['mean_prediction'] = np.mean(grades_prediction)
+        out_info['deviation_prediction'] = np.std(grades_prediction)
 
     if graphic_type in ['scatter', 'difference', 'values']:
         fig = plt.figure()
@@ -136,7 +147,7 @@ def prediction(subject, distance_name, prediction_type, value_used, style_filter
             plt.legend(loc=1)
 
         if graphic_save:
-            plt.savefig(graphics_folder_name + "/prediction_%d_%s_%s_%d_%s_%s_%s.png" % (subject, value_used, filter_file, quant_similar, distance_name, prediction_type, graphic_type))
+            plt.savefig(graphics_folder_name + "/prediction_%d_%s_%s_%d_%s_%s_%s.png" % (subject, value_used, filter_file, quant_similar, distance_name, prediction_weight, graphic_type))
         if graphic_show:
             plt.show()
 
@@ -147,7 +158,7 @@ def prediction(subject, distance_name, prediction_type, value_used, style_filter
         DEBUG_students = array_interleave((DEBUG_students_id, DEBUG_grades_prediction, DEBUG_students_distance), axis=1)
 
         DEBUG_print = np.concatenate([filtered_id[..., np.newaxis], filtered_grades[..., np.newaxis], DEBUG_students, grades_prediction[..., np.newaxis], grades_difference[..., np.newaxis]], axis=1)
-        save_csv(csv_folder_name + "/prediction_%d_%s_%s_%d_%s_%s_%s.csv" % (subject, value_used, filter_file, quant_similar, distance_name, prediction_type, csv_type), DEBUG_print)
+        save_csv(csv_folder_name + "/prediction_%d_%s_%s_%d_%s_%s_%s.csv" % (subject, value_used, filter_file, quant_similar, distance_name, prediction_weight, csv_type), DEBUG_print)
 
 
 # tests_subject = [1, 2, 4, 5, 7, 8]
@@ -155,43 +166,47 @@ def prediction(subject, distance_name, prediction_type, value_used, style_filter
 # tests_value_used = ['grade', 'ability', 'improvement']
 # tests_style_filter = [[], ['ati'], ['ref'], ['sem'], ['int'], ['vis'], ['ver'], ['seq'], ['glo']]
 # tests_quant_similar = [3, 4, 5]
-# tests_prediction_type = ['mean', 'weighted']
-
-tests_subject = [1, 2, 4, 5, 7, 8]
-tests_distance_name = ['cosine']
-tests_value_used = ['grade', 'improvement']
-tests_style_filter = [[], ['ati'], ['ref'], ['sem'], ['int'], ['vis'], ['ver'], ['seq'], ['glo']]
-tests_quant_similar = [3, 4, 5]
-tests_prediction_type = ['mean', 'weighted']
+# tests_prediction_weight = ['uniform', 'distance']
+# tests_prediction_type = ['manual', 'kneighbors', 'gradient']
 
 # tests_subject = [1, 2, 4, 5, 7, 8]
 # tests_distance_name = ['cosine']
-# tests_value_used = ['improvement']
-# tests_style_filter = [[]]
-# tests_quant_similar = [3]
-# tests_prediction_type = ['mean', 'weighted']
+# tests_value_used = ['grade', 'improvement']
+# tests_style_filter = [[], ['ati'], ['ref'], ['sem'], ['int'], ['vis'], ['ver'], ['seq'], ['glo']]
+# tests_quant_similar = [3, 4, 5]
+# tests_prediction_weight = ['uniform', 'distance']
+
+tests_subject = [1]
+tests_distance_name = ['cosine']
+tests_value_used = ['improvement']
+tests_style_filter = [[]]
+tests_quant_similar = [1]
+tests_prediction_weight = ['uniform', 'distance']
+# tests_prediction_type = ['manual', 'kneighbors', 'gradient']
 
 a = 0
-total = len(tests_subject) * len(tests_distance_name) * len(tests_prediction_type) * len(tests_value_used) * len(tests_style_filter) * len(tests_quant_similar)
+total = len(tests_subject) * len(tests_distance_name) * len(tests_prediction_weight) * len(tests_value_used) * len(tests_style_filter) * len(tests_quant_similar)
 
 results = []
-results.append(['Disciplina', 'Tipo de distância', 'Tipo de valor', 'Filtro de estilo', 'Nº Alunos previsão', 'Tipo de previsão', 'Quantidade de alunos', 'Correlação', 'Diferença média', 'Erro', 'Média nota', 'Desvio padrão nota', 'Média previsão', 'Desvio padrão previsão'])
+results.append(['Disciplina', 'Tipo de distância', 'Tipo de valor', 'Filtro de estilo', 'Nº Alunos previsão', 'Tipo de previsão', 'Quantidade de alunos', 'Correlação', 'Diferença média', 'Erro', 'Média habilidade', 'Desvio habilidade', 'Média nota', 'Desvio nota', 'Média previsão', 'Desvio previsão'])
 
 for subject in tests_subject:
     for distance_name in tests_distance_name:
         for value_used in tests_value_used:
             for style_filter in tests_style_filter:
                 for quant_similar in tests_quant_similar:
-                    for prediction_type in tests_prediction_type:
+                    for prediction_weight in tests_prediction_weight:
                         filter_name = ';'.join(style_filter) or 'all'
-                        new_result = [str(subject), distance_name, value_used, filter_name, str(quant_similar), prediction_type]
+                        new_result = [str(subject), distance_name, value_used, filter_name, str(quant_similar), prediction_weight]
 
                         out_info = {}
-                        prediction(subject, distance_name, prediction_type, value_used, style_filter, quant_similar, out_info)
+                        prediction(subject, distance_name, prediction_weight, value_used, style_filter, quant_similar, out_info)
                         new_result.append(out_info['quant_students'])
                         new_result.append("%7.3f" % out_info['correlation'])
                         new_result.append("%7.3f" % out_info['mean_difference'])
                         new_result.append("%7.3f" % out_info['error'])
+                        new_result.append("%7.3f" % out_info['mean_ability'])
+                        new_result.append("%7.3f" % out_info['deviation_ability'])
                         new_result.append("%7.3f" % out_info['mean_grade'])
                         new_result.append("%7.3f" % out_info['deviation_grade'])
                         new_result.append("%7.3f" % out_info['mean_prediction'])
@@ -202,4 +217,4 @@ for subject in tests_subject:
                         a += 1
                         print('%d / %d' % (a, total))
 
-np.savetxt('results/2019-03-25 - Média e desvio/results.csv', results, fmt="%s", delimiter=", ", encoding="utf8")
+np.savetxt(base_path + 'results.csv', results, fmt="%s", delimiter=", ", encoding="utf8")
